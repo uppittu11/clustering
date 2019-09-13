b732b6de642b69b2c83aa28e75c4323d19503254
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-09-13 14:47:43 -0500

    added init.py and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys
+
+try:
+    import mdtraj
+except ImportError:
+    print('Building and running clustering requires mdtraj. See '
+          'http://mdtraj.org/latest/installation.html for help!')
+    sys.exit(1)
+
+setup(name='clustering',
+      version='0.1',
+      description=("Cluster analysis tool for heterogenous lipid ' +
+                   'membranes.'),
+      url='https://github.com/uppittu11/clustering',
+      author='Parashara Shamaprasad',
+      author_email='p.shama@vanderbilt.edu',
+      license='MIT',
+      packages=['clustering.clustering'],
+      package_dir={'clustering': 'clustering/'},
+      include_package_data=True,
+      install_requires=["mdtraj"],
+)
